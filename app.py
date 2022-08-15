6fb559abb14ca9e13b7447cfb79ae428920a8497
Sasha Czerniawski <sashacandoit@gmail.com>
2022-08-15 11:35:30 -0400

    database uri added

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import os
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'postgresql:///capstone_1_db')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///capstone_1_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
